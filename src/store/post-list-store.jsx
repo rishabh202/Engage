401e32bdd42e8013c19c87c0c0e052bb2a801548
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-03-24 16:29:55 +0530

    regular commit- updated UI

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,34 +1,53 @@
 import { createContext, useReducer } from "react";
-import { DefaultContext } from "react-icons";
 
-const DEFAULT_CONTEXT = {
-    
-        postList: [],
-        addPost: () => {},
-        deletePost: () => {}
-    
-    
-}
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
 
-const PostList = createContext(DEFAULT_CONTEXT);
-
- const postListReducer = (currPostList, action) => {
+const postListReducer = (currPostList, action) => {
   return currPostList;
- }
-
-const PostListProvider = (children) => {
-  const[postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_CONTEXT) 
-  
-  const addPost = () =>{
-    
-  }
-  const deletePost = () =>{
-
-  }
-
-   return <PostList.Provider value={
-    {postList, addPost, deletePost}
-   }>{children}</PostList.Provider>;
 };
+
+const PostListProvider = ({children}) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+
+  const addPost = () => {};
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+        {
+        id: '1',
+        title: "Going to Delhi", 
+        body: "Hi i am going to delhi for my nephews wedding. Really excited to attend the wedding fucntion!!!", 
+        reactions: 2,
+        userId: "user-9",
+        tags: ["vacations", "Delhi", "Enjoying"]
+ },
+        {
+        id: '2',
+        title: "Going to Delhi", 
+        body: "Hi i am going to delhi for my nephews wedding. Really excited to attend the wedding fucntion!!!", 
+        reactions: 2,
+        userId: "user-16",
+        tags: ["vacations", "Delhi", "Enjoying"]
+ },
+        {
+        id: '3',
+        title: "Going to Delhi", 
+        body: "Hi i am going to delhi for my nephews wedding. Really excited to attend the wedding fucntion!!!", 
+        reactions: 2,
+        userId: "user-13",
+        tags: ["vacations", "Delhi", "Enjoying"]
+ }
+];
 
 export default PostListProvider;
